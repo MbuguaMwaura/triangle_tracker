f715a45844dbc9c3214ca316f8c3698eb073a30f
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-03-23 19:57:39 +0300

    made changes on the js file in order to enable the function to run.ie added []

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
+
+function tracker() {
 var length1 = document.getElementById("length1").value;
 var length2 = document.getElementById("length2").value;
 var length3 = document.getElementById("length3").value;
 
-var x = parseFloat(length1);
-var y = parseFloat(length2);
-var z = parseFloat(length3);
+var x = [parseFloat(length1)];
+var y = [parseFloat(length2)];
+var z = [parseFloat(length3)];
 
  var ab = x.concat(y);
 
@@ -12,25 +14,23 @@ var z = parseFloat(length3);
 
 var sorted = abc.sort(function (d, e) {
 return d - e;
-})
+});
 
-var a = sorted[0]
-var b = sorted[1]
-var c = sorted[2]
+var a = sorted[0];
+var b = sorted[1];
+var c = sorted[2];
 
 var lengthab = a + b;
 
 
 if(lengthab < c){
-  	alert("ERROR! Not a triangle")
+  	alert("ERROR! Not a triangle");
   }
 else if(a === b && b === c && a > 0 && b > 0 && c > 0){
-  console.log('Equilateral Triangle')
-  alert('Equilateral Triangle')
+  alert('Equilateral Triangle');
 }else if(a === b && b !== c || a !== b && b === c && a > 0 && b > 0 && c > 0){
-  console.log('Isosceles Triangle')
-  alert('Isoceles Triangle')
+  alert('Isoceles Triangle');
 }else if(a !== b && b!== c && (Number.isInteger(a) === true || Number.isInteger(b) === true || Number.isInteger(c) === true) && a > 0 && b > 0 && c > 0){
-  console.log('Scalene Triangle')
-  alert('Scalene Triangle')
+  alert('Scalene Triangle');
 }else { alert ("Invalid measurements. Kindly re-evaluate your figures.")};
+}
