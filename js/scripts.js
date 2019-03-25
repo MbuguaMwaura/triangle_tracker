9a0eaed1297018d50e2a00a5fc540e74440f2e81
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-03-25 12:39:08 +0300

    indented all the code appropriately and change the values on the media queries

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,41 @@
-
 function tracker() {
-var length1 = document.getElementById("length1").value;
-var length2 = document.getElementById("length2").value;
-var length3 = document.getElementById("length3").value;
+  var length1 = document.getElementById("length1").value;
+  var length2 = document.getElementById("length2").value;
+  var length3 = document.getElementById("length3").value;
 
-var x = [parseFloat(length1)];
-var y = [parseFloat(length2)];
-var z = [parseFloat(length3)];
+  var x = [parseFloat(length1)];
+  var y = [parseFloat(length2)];
+  var z = [parseFloat(length3)];
 
- var ab = x.concat(y);
+  var ab = x.concat(y);
 
- var abc = ab.concat(z) ;
+  var abc = ab.concat(z);
 
-var sorted = abc.sort(function (d, e) {
-return d - e;
-});
+  var sorted = abc.sort(function (d, e) {
+    return d - e;
+  });
 
-var a = sorted[0];
-var b = sorted[1];
-var c = sorted[2];
+  var a = sorted[0];
+  var b = sorted[1];
+  var c = sorted[2];
 
-var lengthab = a + b;
+  var lengthab = a + b;
 
 
-if(lengthab < c){
-  document.getElementById("output").innerHTML = "Error, not a triangle";
-  }
-else if(a === b && b === c && a > 0 && b > 0 && c > 0){
-  document.getElementById("output").innerHTML = "Equilateral Triangle.";
-}else if(a === b && b !== c || a !== b && b === c && a > 0 && b > 0 && c > 0){
-  document.getElementById("output").innerHTML = "Isosceles Triangle";
-}else if(a !== b && b!== c && (Number.isInteger(a) === true || Number.isInteger(b) === true || Number.isInteger(c) === true) && a > 0 && b > 0 && c > 0){
-  document.getElementById("output").innerHTML = "Scalene Triangle";;
-}else { document.getElementById("output").innerHTML = "Invalid Measurements";};
+  if (lengthab < c) {
+    document.getElementById("output").innerHTML = "Error, not a triangle";
+  } else if (a === b && b === c && a > 0 && b > 0 && c > 0) {
+    document.getElementById("output").innerHTML = "Equilateral Triangle.";
+  } else if (a === b && b !== c || a !== b && b === c && a > 0 && b > 0 && c > 0) {
+    document.getElementById("output").innerHTML = "Isosceles Triangle";
+  } else if (a !== b && b !== c && (Number.isInteger(a) === true || Number.isInteger(b) === true || Number.isInteger(c) === true) && a > 0 && b > 0 && c > 0) {
+    document.getElementById("output").innerHTML = "Scalene Triangle";;
+  } else {
+    document.getElementById("output").innerHTML = "Invalid Measurements";
+  };
 }
 
-function reset(){
+function reset() {
   document.getElementById("myform").reset();
   document.getElementById("output").innerHTML = "Try other figures";
 }
